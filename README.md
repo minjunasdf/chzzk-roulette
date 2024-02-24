d823f04dbdfe8e0e81c127cf0698c306457a386f
Malloc314159 <malloc_3141592@naver.com>
2024-02-24 23:03:34 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# 치지직 도네이션 룰렛
+
+[kimcore](https://github.com/kimcore)님의 [치지직 api](https://github.com/kimcore/chzzk)와 [Buddha7771](https://github.com/Buddha7771)님의 [치지직 채팅 크롤링](https://github.com/Buddha7771/ChzzkChat) 코드를 참고하여 제작하였습니다.
+
+## 설치
+
+---
+```bash
+$ git clone https://github.com/Malloc314159/chzzk-roulette
+$ cd chzzk-roulette
+
+$ conda create -n chzzk python=3.11
+$ conda activate chzzk
+
+$ pip install websocket
+$ pip install websocket-client
+$ pip install requests
+```
+
+## 쿠키 값 찾는 법
+
+---
+1. [치지직](https://chzzk.naver.com) 접속
+2. F12를 눌러 개발자 도구 접속 후 쿠키 탭에 들어가 'NID_AUT'와 'NID_SES' 값을 찾습니다.
+3. 해당 값을 main.py에 붙여 넣습니다.
+
+## 실행
+
+---
+```commandline
+python main.py
+
+python main.py --streamer_id (스트리머 ID)
+```
---
+++ main.py
@@ -0,0 +1,217 @@
+import argparse
+import datetime
+import logging
+import json
+import random as rd
+from websocket import WebSocket
+
+from chat_cmd import CHAT_CMD
+import api
+
+class ChzzkChat:
+
+    def __init__(self, streamer, cookies, logger):
+
+        self.streamer = streamer
+        self.cookies = cookies
+        self.logger = logger
+
+        self.sid = None
+        self.userIdHash = api.fetch_userIdHash(self.cookies)
+        self.chatChannelId = api.fetch_chatChannelId(self.streamer)
+        self.channelName = api.fetch_channelName(self.streamer)
+        self.accessToken, self.extraToken = api.fetch_accessToken(self.chatChannelId, self.cookies)
+
+        self.connect()
+
+    def connect(self):
+
+        sock = WebSocket()
+        sock.connect('wss://kr-ss1.chat.naver.com/chat')
+        print(f'{self.channelName} 채팅창에 연결 중 .', end="")
+
+        default_dict = {
+            "ver": "2",
+            "svcid": "game",
+            "cid": self.chatChannelId,
+        }
+
+        send_dict = {
+            "cmd": CHAT_CMD['connect'],
+            "tid": 1,
+            "bdy": {
+                "uid": self.userIdHash,
+                "devType": 2001,
+                "accTkn": self.accessToken,
+                "auth": "SEND"
+            }
+        }
+
+        sock.send(json.dumps(dict(send_dict, **default_dict)))
+        sock_response = json.loads(sock.recv())
+        self.sid = sock_response['bdy']['sid']
+        print(f'\r{self.channelName} 채팅창에 연결 중 ..', end="")
+
+        send_dict = {
+            "cmd": CHAT_CMD['request_recent_chat'],
+            "tid": 2,
+
+            "sid": self.sid,
+            "bdy": {
+                "recentMessageCount": 50
+            }
+        }
+
+        sock.send(json.dumps(dict(send_dict, **default_dict)))
+        sock.recv()
+        print(f'\r{self.channelName} 채팅창에 연결 중 ...')
+
+        self.sock = sock
+        if self.sock.connected:
+            print('연결 완료')
+        else:
+            raise ValueError('오류 발생')
+
+    def send(self, message: str):
+
+        default_dict = {
+            "ver": 2,
+            "svcid": "game",
+            "cid": self.chatChannelId,
+        }
+
+        extras = {
+            "chatType": "STREAMING",
+            "emojis": "",
+            "osType": "PC",
+            "extraToken": self.extraToken,
+            "streamingChannelId": self.chatChannelId
+        }
+
+        send_dict = {
+            "tid": 3,
+            "cmd": CHAT_CMD['send_chat'],
+            "retry": False,
+            "sid": self.sid,
+            "bdy": {
+                "msg": message,
+                "msgTypeCode": 1,
+                "extras": json.dumps(extras),
+                "msgTime": int(datetime.datetime.now().timestamp())
+            }
+        }
+
+        self.sock.send(json.dumps(dict(send_dict, **default_dict)))
+
+    def run(self):
+        while True:
+            try:
+                try:
+                    raw_message = self.sock.recv()
+                except:
+                    self.connect()
+                    raw_message = self.sock.recv()
+
+                raw_message = json.loads(raw_message)
+                chat_cmd = raw_message['cmd']
+
+                if chat_cmd == CHAT_CMD['ping']:
+                    self.sock.send(
+                        json.dumps({
+                            "ver": "2",
+                            "cmd": CHAT_CMD['pong']
+                        })
+                    )
+                    continue
+
+                if chat_cmd != CHAT_CMD['donation']:
+                    continue
+
+                # print(raw_message['bdy'])
+
+                for chat_data in raw_message['bdy']:
+
+                    if chat_data['uid'] == 'anonymous':
+                        nickname = '익명의 후원자'
+
+                    else:
+                        try:
+                            profile_data = json.loads(chat_data['profile'])
+                            extra_data = json.loads(chat_data['extras'])
+                            nickname = profile_data["nickname"]
+                            donated = extra_data["payAmount"]
+
+                        except:
+                            print('error')
+                            continue
+
+                    now = datetime.datetime.fromtimestamp(chat_data['msgTime'] / 1000)
+                    now = datetime.datetime.strftime(now, '%Y-%m-%d %H:%M:%S')
+
+                    self.logger.info(f'[{now}] {nickname} : {chat_data["msg"]} / {donated}')
+
+                    print(donated)
+
+                    #if '룰렛' in chat_data['msg']:
+                    rouletteCount=0
+                    if donated >= 10000:
+                        rouletteCount = donated // 10000
+                        for i in range(rouletteCount):
+                            result=self.roulette(2)
+                            print(result)
+                    else:
+                        rouletteCount = donated // 1000
+                        for i in range(rouletteCount):
+                            result=self.roulette(1)
+                            print(result)
+
+            except:
+                print('error from run function')
+                pass
+
+    def roulette(self, rNum):
+        r1dict={'방종': 0.1, '방송연장': 0.1, '꽝': 0.8}
+        r2dict={'방종': 0.05, '방송연장': 0.25, '꽝': 0.2, '노래방': 0.3, '소원권': 0.2}
+        randomNum=rd.random()
+        if rNum==1:
+            selectedDict=r1dict
+        else:
+            selectedDict=r2dict
+        tmpSum=0
+        for k, v in selectedDict.items():
+            tmpSum+=v
+            if randomNum<=tmpSum:
+                return k
+
+
+def get_logger():
+    formatter = logging.Formatter('%(message)s')
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler('chat.log', mode="w")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--streamer_id', type=str, default='6e06f5e1907f17eff543abd06cb62891')
+    args = parser.parse_args()
+
+    cookies = {
+        "NID_AUT" : "Put your AUT Cookie HERE",
+        "NID_SES" : "Put your SES Cookie HERE"
+    }
+
+    logger = get_logger()
+    chzzkchat = ChzzkChat(args.streamer_id, cookies, logger)
+
+    chzzkchat.run()
